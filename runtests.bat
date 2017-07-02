61e3767cab6d95c32d150d2f5561a52677446272
Valentin Konovalov <valle.ketsujin@gmail.com>
2017-07-02 11:21:52 -0700

    buildTools 25.0.3

--- runtests.bat
+++ runtests.bat
@@ -1,2 +1,2 @@
-call gradlew uninstallAll installRelease installReleaseAndroidTest connectedAndroidTest
+call gradlew clean uninstallAll installRelease installReleaseAndroidTest connectedAndroidTest
 rem to run in debug  mode rename signing.properties file
